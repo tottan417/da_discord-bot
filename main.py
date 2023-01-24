04d308ac3d4a5a422d57b9ed3aa44ef7327fef46
tottan <t_izaki@outlook.jp>
2023-01-24 17:37:17 +0900

    test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # インストールした discord.py を読み込む
+from discord.ext import tasks
 import discord
 import config
+import time
 
 # 自分のBotのアクセストークンに置き換えてください
 TOKEN = config.TOKEN
@@ -23,6 +25,10 @@ async def on_message(message):
     # 「/neko」と発言したら「にゃーん」が返る処理
     if message.content == '/neko':
         await message.channel.send('にゃーん')
+
+@tasks.loop(seconds=10)
+async def send_message_every_10sec():
+    print(time.time())
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
